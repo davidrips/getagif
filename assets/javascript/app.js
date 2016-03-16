f739e678fe6482cc91d6696a2df241aa4e354fb4
David Rips <davidrips@github.com>
2016-03-16 04:41:24 -0400

    added functionality, to be cleaned

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
         for (var i=0; i<searches.length; i++){
             var a= $('<button>');
             a.addClass('search');
-            a.attr('data-animal', searches[i]);
+            a.attr('data-search', searches[i]);
             a.text(searches[i]);
             $('#buttonshere').append(a);
         }
     };
 
-
     renderButtons();
+
     $('#addnew').on('click', function(){
         var newsearch = $('#newcat').val().trim();
         searches.push(newsearch);
@@ -23,12 +23,45 @@ $(document).ready(function(){
         renderButtons();
         return false
     })
+    
+    //Clean this up by making all the parts of the if conditions into global vars//
+   
+   function ratingset(){
 
+         if ( ($("#r").is(':checked') == false) && ($("#pg13").is(':checked') == false) && ($("#pg").is(':checked') == false) ){
+                ratingSelected = 'g'
+            } else if (($("#r").is(':checked') == false) && ($("#pg13").is(':checked') == false) && ($("#pg").is(':checked') == true)){
+                ratingSelected = 'pg';
+            } else if (($("#r").is(':checked') == false) && ($("#pg13").is(':checked') == true) && ($("#pg").is(':checked') == true)){
+                ratingSelected = 'pg-13'
+            } else if (($("#r").is(':checked') == true) && ($("#pg13").is(':checked') == true) && ($("#pg").is(':checked') == true)){
+                ratingSelected = 'r'
+            }
+             console.log(ratingSelected);
+    };
 
-    $(document).on('click', '.search', function() {
-        $('#gifsAppearHere').empty();
-        var animal = $(this).data('animal');
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+    ratingset();
+
+//gotta figure out how to make a doIt() work with the previously clicked event... ///
+    $("#ratingssubmit").on('click', function(){
+        ratingset();
+        doIt(aTest);
+
+        console.log("shouldbe")
+        console.log(ratingSelected)
+        console.log(aTest);
+        return false
+
+    })
+//maybe by defining 'this' as an argument in the doIt function, and then having the original onclick set a value to it? such as, onclick makes test = $(this) whatever it is on the click, and then run doIt(test). 
+
+    
+    function doIt(it) {
+         $('#gifsAppearHere').empty();
+        var query = it;
+        
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&rating="+ ratingSelected;
+
 
         $.ajax({
                 url: queryURL,
@@ -40,21 +73,21 @@ $(document).ready(function(){
 
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
-                var animalDiv = $('<div>');
+                var gifDiv = $('<div>');
                 var p = $('<p>');
                 p.text("Rating: "+results[i].rating);
-                var animalImage = $('<img>');
-                animalImage.attr('src',results[i].images.fixed_height_still.url);
-                animalImage.addClass('animalImage')
-                animalImage.attr('data-still',results[i].images.fixed_height_still.url);
-                animalImage.attr('data-animate',results[i].images.fixed_height.url);
-                animalImage.attr('data-state','still');
-                animalDiv.append(p);
-                animalDiv.append(animalImage)
-                $('#gifsAppearHere').prepend(animalDiv);
+                var gifImage = $('<img>');
+                gifImage.attr('src',results[i].images.fixed_height_still.url);
+                gifImage.addClass('gifImage')
+                gifImage.attr('data-still',results[i].images.fixed_height_still.url);
+                gifImage.attr('data-animate',results[i].images.fixed_height.url);
+                gifImage.attr('data-state','still');
+                gifDiv.append(p);
+                gifDiv.append(gifImage)
+                $('#gifsAppearHere').prepend(gifDiv);
             }
 
-            $('.animalImage').on('click', function (){
+            $('.gifImage').on('click', function (){
                 var state = $(this).attr('data-state');
                 if ( state == 'still'){
                     $(this).attr('src', $(this).data('animate'));
@@ -65,5 +98,15 @@ $(document).ready(function(){
                 }
             })  
         });
+
+    }
+    var aTest = 0
+
+    $(document).on('click', '.search', function (){
+        aTest = $(this).data('search');
+        console.log(aTest);
+        doIt(aTest);
+
+
     });
 })
